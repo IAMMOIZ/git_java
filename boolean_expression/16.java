85a8519053226e35388b73a1bcf20c96e468a6de
Sohail Khan <MoizAnsari1796@gmail.com>
2019-10-22 22:00:37 +0530

    some new files

---
+++ boolean_expression/16.java
@@ -0,0 +1,11 @@
+//boolean expression experiment
+class A
+{
+public static void main(String gg[])
+{
+int i =0;
+boolean b1 = ((i++ == 0) || (i++ ==1));
+System.out.println(i);
+System.out.println(b1);
+}
+}
